0dff7ae481c0e16e3e0edbb43cb94dba78a8eac4
Rocky <rrsalerno21@gmail.com>
2020-03-31 15:25:03 -0700

    added search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,6 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    try {
-        const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`)
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch(error) {
-        alert(error)
-    }
-}
-getResults('pasta');
+const search = new Search('pizza');
+console.log(search);
+
+search.getResults();
